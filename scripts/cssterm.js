7595a9b2a490e33b31912d10b28a25f2f74f698b
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2013-06-29 17:53:11 +0400

    refract

--- scripts/cssterm.js
+++ scripts/cssterm.js
@@ -2,11 +2,9 @@ $(document).ready(function() {
   // these are the special characters the javascript will look for at the beginning of a line
   var rstr = /^#/gi;  // indicates a root prompt
   var ustr = /^\$/gi;  // indicates a user prompt
-  var sstr = /^>/gi;  // indicates standard output
 
-  var rprompt = "[root@localhost]# ";  // will replace occurrences of rstr
-  var uprompt = "[user@localhost]$ ";  // will replace occurrences of ustr
-  var stdout = "&nbsp;&nbsp;";  // will replace occurrences of sstr
+  var rprompt = "<root>[root@localhost]# </root>";  // will replace occurrences of rstr
+  var uprompt = "<user>[user@localhost]$ </user>";  // will replace occurrences of ustr
 
   // the html that will be inserted to replace the shortened code
   // the terminal bar and body before the text is placed
@@ -44,15 +42,14 @@ $(document).ready(function() {
     var newString = "";
     jQuery.each(arrayContent, function() {
       // make sure there's text to avoid blank spaces
-      if (this.length != 0 ) {
+      if (this.length != 0) {
         // is string a root command
         if (this.charAt(0) == "#") {
           newString += this.replace(rstr, rprompt).concat("<br>\n");
-          // is string a comment (don't forget to close that div)
         } else if (this.charAt(0) == "$") {
           newString += this.replace(ustr, uprompt).concat("<br>\n");
         } else {
-          newString += stdout + this + "<br>\n";
+          newString += this + "<br>\n";
       }
     }
   });
